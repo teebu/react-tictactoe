481cf70eb035fa411031b45d06ef64dfb5eefef9
teebu <teebu@users.noreply.github.com>
2020-04-26 00:20:35 +0000

    deploy: b006af4c348460fdbc80c67bc99714d04b8de6e0

---
+++ precache-manifest.5ba360e4cce870a099ca034b7ce0f4a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48b523a93877d4422d2cef510f7e3c6",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "5b82230ad8910570a5c6",
+    "url": "/react-tictactoe/static/css/main.f381d09e.chunk.css"
+  },
+  {
+    "revision": "dd06a52d4ade1d9f6f3d",
+    "url": "/react-tictactoe/static/js/2.72874d66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tictactoe/static/js/2.72874d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b82230ad8910570a5c6",
+    "url": "/react-tictactoe/static/js/main.77217c4e.chunk.js"
+  },
+  {
+    "revision": "bb0236b7a9424db6e69f",
+    "url": "/react-tictactoe/static/js/runtime-main.1a451a50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tictactoe/static/media/logo.5d5d9eef.svg"
+  }
+]);
